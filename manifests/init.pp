76e55e0b292f713e34573277b5a9a0f558f0f809
David Wilcox <david.wilcox@qa.com>
2016-06-15 12:35:19 +0100

    XX

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,4 @@
 class env_notify_module () {
+  notify{"This a feature, I'm still fiddling with this module": }
   notify{"This module is causing a horrendous bug in environment : $::environment": }
 }
